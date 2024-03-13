afc4943f6bfe81194f06f02593f8f401ab868055
Devansh Khare <devanshkhare0@gmail.com>
2024-03-13 07:17:40 +0530

    completed community

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,9 +1,11 @@
+import CommunityCard from "@/components/cards/CommunityCard";
 import UserCard from "@/components/cards/UserCard";
 import PostThread from "@/components/forms/PostThread";
 import ProfileHeader from "@/components/shared/ProfileHeader";
 import ThreadsTabs from "@/components/shared/ThreadsTab";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { profileTabs } from "@/constants";
+import { fetchCommunities } from "@/lib/actions/community.actions";
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import Image from "next/image";
@@ -14,30 +16,31 @@ const Page = async () => {
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
-  const result = await fetchUsers({
-    userId: user.id,
+  const result = await fetchCommunities({
     searchString: '',
     pageNumber: 1,
     pageSize: 25,
     sortBy: 'desc'
   })
+
   return (
     <section>
       <h1 className="head-text-mb-10">Search</h1>
       <div className="mt-14 flex flex-col gap-9">
-        {result.users.length === 0 ? (
-            <p className="no-result">No Users</p>
+        {result.communities.length === 0 ? (
+            <p className="no-result">No communities</p>
         ):
         
         <>
-            {result.users.map((person)=>(
-                <UserCard
-                key={person.id}
-                id={person.id}
-                name={person.name}
-                username={person.username}
-                imageUrl={person.image}
-                personType='User'
+            {result.communities.map((community)=>(
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
                 />
             ))}
         </>
