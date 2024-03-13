769ea067926b979b1957ab17e7a01a30e6ed46d6
Devansh Khare <devanshkhare0@gmail.com>
2024-03-13 17:26:49 +0530

    search integration

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,18 +1,45 @@
-import SearchSection from "@/components/search/SearchSection";
-import { fetchUser } from "@/lib/actions/user.actions";
+import UserCard from "@/components/cards/UserCard";
+import SearchBar from "@/components/shared/SearchBar";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-const Page = async () => {
+const Page = async ({searchParams}: {searchParams: {q: string}}) => {
   const user = await currentUser();
   if (!user) return null;
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: searchParams?.q,
+    pageNumber: 1,
+    pageSize: 25,
+    sortBy: 'desc'
+  })
+
   return (
     <section>
       <h1 className="head-text-mb-10">Search</h1>
-      <SearchSection id={user?.id} />
+      <SearchBar page="search"/>
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+            <p className="no-result">No Users</p>
+        ):
+        <>
+            {result.users.map((person)=>(
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imageUrl={person.image}
+                personType='User'
+                />
+            ))}
+        </>
+        }
+      </div>
     </section>
   );
 };
