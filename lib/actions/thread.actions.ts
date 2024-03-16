eb03813d13e6b92ab8e9c1ae1efedf09101bdde7
Devansh Khare <devanshkhare0@gmail.com>
2024-03-16 15:35:42 +0530

    added revalidation

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -163,6 +163,7 @@ async function generateIds(threadId: string, threadsToBeDeleted: any){
   }
   const originalThread = await Thread.findById(threadId);
   if (originalThread.children.length > 0) {
+    console.log("originalThread", originalThread)
     await Promise.all(originalThread.children.map(async (child: any) => {
       threadsToBeDeleted.push(child._id.toString());
       await generateIds(child._id, threadsToBeDeleted);
@@ -186,6 +187,7 @@ export async function deleteThread(threadId: string) {
     if(threadToUnlinkFromUser.length > 0) {
       await User.updateMany({}, {$pull: { threads: threadToUnlinkFromUser}})
     }
+    revalidatePath("/");
   } catch (error) {
   }
 }
