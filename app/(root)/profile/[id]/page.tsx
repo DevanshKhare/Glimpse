209f658f8950402a0c65b9dd2adf98bcf4a3ee3f
Devansh Khare <devanshkhare0@gmail.com>
2024-03-20 16:56:50 +0530

    introduced infinite loading on user profile

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -24,26 +24,37 @@ const Page = async ({ params }: { params: { id: string } }) => {
       />
       <div className="mt-9">
         <Tabs defaultValue="threads" className="w-full">
-            <TabsList className="tab">
-                {profileTabs.map((tab) => (
-                    <TabsTrigger key={tab.label} value={tab.value} className="tab">
-                        <Image
-                            src={tab.icon}
-                            width={24}
-                            height={24}
-                            alt={tab.label}
-                            className="object-contain"
-                        />
-                        <p className="max-sm:hidden">{tab.label}</p>
-                        {tab.label === 'Threads' && (
-                            <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 text-tiny-medium text-light-2">
-                                {userInfo?.threads?.length}
-                            </p>
-                        )}
-                    </TabsTrigger>
-                ))}
-            </TabsList>
-          <RenderThreadsTabSection userId={user.id} accountId={userInfo.id} />
+          <TabsList className="tab">
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className="tab">
+                <Image
+                  src={tab.icon}
+                  width={24}
+                  height={24}
+                  alt={tab.label}
+                  className="object-contain"
+                />
+                <p className="max-sm:hidden">{tab.label}</p>
+                {tab.label === "Threads" && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 text-tiny-medium text-light-2">
+                    {userInfo?.threads?.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          {profileTabs.map((tab) => (
+            <TabsContent
+              key={`content-${tab.label}`}
+              value={tab.value}
+              className="w-full text-light-1"
+            >
+              <RenderThreadsTabSection
+                userId={user.id}
+                accountId={userInfo.id}
+              />
+            </TabsContent>
+          ))}
         </Tabs>
       </div>
     </section>
