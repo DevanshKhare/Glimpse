195afd693f3f9ee0a8657f09d18209c2517204de
Devansh Khare <devanshkhare0@gmail.com>
2024-03-21 16:38:35 +0530

    reverted changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,32 +1,19 @@
-import SingleLineThreadCreatev2 from "@/components/forms/SingleLineThreadCreatev2";
-import ThreadsSectionNew from "@/components/sections/ThreadsSectionNew";
 import { fetchThreads } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-
+import ThreadsSectionWithoutLazy from "@/components/sections/ThreadsSectionWithoutLazy";
 export default async function Home() {
   const user = await currentUser();
   if(!user) return null;
   const userInfo = await fetchUser(user?.id || "");
   if (!userInfo?.onboarded) redirect("/onboarding");
-  const { threads } = await fetchThreads(0, 4);
 
   return (
     <>
-      {/* <RenderThreadsSection
+      <ThreadsSectionWithoutLazy
         user={JSON.parse(JSON.stringify(user))}
         userInfo={userInfo}
-      /> */}
-      <SingleLineThreadCreatev2
-        userId={userInfo?._id}
-        user={JSON.parse(JSON.stringify(user))}
-        userInfo={userInfo}
-      />
-      <ThreadsSectionNew
-        user={JSON.parse(JSON.stringify(user))}
-        userInfo={userInfo}
-        initialThreads={threads}
       />
     </>
   );
